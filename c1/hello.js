4a5ade173b3ef03504ab3ba76833fb5bd0bc5ada
Chibikuri <ryosuke.satoh.wk@gmail.com>
2019-04-09 16:15:36 +0900

    add ch1

---
+++ c1/hello.js
@@ -0,0 +1,20 @@
+function setup(){
+	//initialize
+	createCanvas(600, 400)	
+}
+
+//main part of script
+//called 30 times
+function draw(){
+	background(120, 200, 330); //gray
+	//color(240, 120, 0);
+	//rect(10, 10, 50, 60);
+	circle(30, 30, 30);
+	circle(35, 40, 30);
+	circle(40, 50, 30);
+	circle(45, 60, 30);
+	circle(50, 70, 30);
+	circle(55, 80, 30);
+	circle(60, 90, 30);
+
+}
