be55c701ded44b2f9d809e218d1d9475269cfb8b
Chibikuri <ryosuke.satoh.wk@gmail.com>
2019-04-24 23:22:35 +0900

    3

--- ch03/ch03.js
+++ ch03/ch03.js
@@ -6,27 +6,43 @@ function setup(){
 }
 
 var pos = 0;
+var adj = 50;
+var col = 0;
 function draw(){
-    background(30, 128, 180, 20);
+    col = col+1;
     pos = pos + 1;
-    fill(pos, 256 - pos, 128 + pos/2);
-    circle(pos-random(100), pos-random(100), pos*0.01);
-    circle(pos-random(100), pos-random(100), pos*0.02);
-    circle(pos-random(100), pos-random(100), pos*0.03);
-    circle(pos-random(100), pos-random(100), pos*0.04);
-    fill(256 - pos, pos, 128 + pos/2)
-    circle(width - pos-random(100), pos-random(100), pos*0.01);
-    circle(width - pos-random(100), pos-random(100), pos*0.02);
-    circle(width - pos-random(100), pos-random(100), pos*0.03);
-    circle(width - pos-random(100), pos-random(100), pos*0.04);
+    if(col < 300){
+        background(col-120, 50, 110, 10);
+    }else{
+        background(180, 50, 110, 10);
+    }
+    center = 300
+    if(pos < center){
+        fill((pos%width), 256 - (pos%width), 128 + (pos%width)/2);
+        circle(pos-random(100), pos-random(100), pos*0.01);
+        circle(pos-random(100), pos-random(100), pos*0.02);
+        circle(pos-random(100), pos-random(100), pos*0.03);
+        circle(pos-random(100), pos-random(100), pos*0.04);
+    }else{
+        fill(256 - (pos%width), (pos%width), 128 + (pos%width)/2);
+        circle(pos-random(100), pos-random(100), pos*0.01);
+        circle(pos-random(100), pos-random(100), pos*0.03);
+        fill(pos, 256 - pos, 128 + pos/2);
+        circle(pos-random(100), pos-random(100), pos*0.02);
+        circle(pos-random(100), pos-random(100), pos*0.04);
+    }
+    if(pos < center){
+        fill(256 - (pos%width), (pos%width), 128 + (pos%width)/2);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.01);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.02);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.03);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.04)
+    }else{
+        fill((pos%width), 256 - (pos%width), 128 + (pos%width)/2);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.01);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.03);
+        fill(256 - (pos%width), (pos%width), 128 + (pos%width)/2);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.02);
+        circle(width + adj - pos-random(100), pos-random(100), pos*0.04);
+    }
 }
-
-function shootingstars(){
-    circle(width - pos, pos, pos*0.1);
-    sleep(1);
-    circle(width - pos, pos, pos*0.1);
-    sleep(1);
-    circle(width - pos, pos, pos*0.1);
-    sleep(1);
-    circle(width - pos, pos, pos*0.1);
-    sleep(1);}
